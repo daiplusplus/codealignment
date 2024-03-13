e9bece94b57dbf0eac567f87a10c1df3241c0e80
Dai Rees <dai@dai.me>
2024-03-12 21:57:54 -0700

    Consolidating Resources.resx into VSPackage.resx. Adding colon toolbar button.

--- CodeAlignment.VisualStudio/Resources/VSPackage.Designer.cs
+++ CodeAlignment.VisualStudio/Resources/VSPackage.Designer.cs
@@ -87,5 +87,15 @@ namespace CMcG.CodeAlignment {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap CodeAlignmentVSBanner {
+            get {
+                object obj = ResourceManager.GetObject("CodeAlignmentVSBanner", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
